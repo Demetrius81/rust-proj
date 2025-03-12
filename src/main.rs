307be8b201526f864cc864992a6a301665c42d84
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-03-12 18:09:36 +0300

    feat: add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-use std::net::SocketAddr;
-
 use axum::{response::Html, routing::get, Router};
+use dotenv::dotenv;
+use std::{env, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
+    const VAR_ADDRESS_KEY: &str = "SERVER_ADDRESS";
+    dotenv().ok();
+    
     let app = Router::new().route("/", get(root));
+    let addr = env::var(&VAR_ADDRESS_KEY)
+        .unwrap_or_else(|e| panic!("Failed to get env with name '{}': {}", VAR_ADDRESS_KEY, e));
+    
+    let parset_address: SocketAddr = addr
+        .parse()
+        .unwrap_or_else(|e| panic!("Failed to parse socket address: {}", e));
 
-    let addr = SocketAddr::from(([127.0.0.1], 3000));
-    axum::Server::bind(&addr)
+    axum_server::bind(parset_address)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
 async fn root() -> Html<&'static str> {
-    Html("Hello, world!");
+    Html("Hello, world!")
 }
