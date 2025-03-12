1e683c872bf53b039ca143f4716eda899bf693ab
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-03-12 15:15:59 +0300

    feat: add main and root

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127.0.0.1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> Html<&'static str> {
+    Html("Hello, world!");
+}
