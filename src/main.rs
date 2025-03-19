da31148176a7f9516ea678d886bd26eb0b48c046
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-03-19 10:51:13 +0300

    feat: add struct Route

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use axum::{response::Html, routing::get, Router};
 use dotenv::dotenv;
 use std::{env, net::SocketAddr};
 
+struct Route {
+    path: String,
+    action: String,
+    exec: fn(), 
+}
+
 #[tokio::main]
 async fn main() {
     const VAR_ADDRESS_KEY: &str = "SERVER_ADDRESS";
@@ -10,7 +16,7 @@ async fn main() {
     let app = Router::new().route("/", get(root));
     let addr = env::var(&VAR_ADDRESS_KEY)
         .unwrap_or_else(|e| panic!("Failed to get env with name '{}': {}", VAR_ADDRESS_KEY, e));
-    
+
     let parset_address: SocketAddr = addr
         .parse()
         .unwrap_or_else(|e| panic!("Failed to parse socket address: {}", e));
