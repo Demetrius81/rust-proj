11f0926fdc407fa556b0fa1641858433dd975a2b
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-03-19 12:11:01 +0300

    feat: add simple server

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokyo::maim]
+async fn main() {
+    let routes_hello = Router::new().route("/hello", get(|| async {Html("Hello <strong>World!!!</strong>")}));
+
+    // region:   --- Start server
+
+    let addr = SocketAddr::from(([127,0,0,1], 8080));
+    println!("--->>>LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+
+    // endregion:    --- Start server
+}
