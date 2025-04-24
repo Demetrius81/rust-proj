a6f4d6efe008477a21fb411e8b83acb1834ca338
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-04-24 09:23:36 +0300

    feat: add simple get hello

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,29 @@ use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(|| async {Html("Hello <strong>World!!!</strong>")}));
+    let app = Router::new().route("/hello", get(hello));
 
-    // region:   --- Start server
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    let addr = SocketAddr::from(([127,0,0,1], 8080));
-    println!("--->>>LISTENING on {addr}\n");
-    axum_server::bind(addr)
-        .serve(routes_hello.into_make_service())
-        .await
-        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
-    // endregion:    --- Start server
+// #[tokio::main]
+// async fn main() {
+//     let routes_hello = Router::new().route("/hello", get(|| async {Html("Hello <strong>World!!!</strong>")}));
+
+//     // region:   --- Start server
+
+//     let addr = SocketAddr::from(([127,0,0,1], 8080));
+//     println!("--->>>LISTENING on {addr}\n");
+//     axum_server::bind(addr)
+//         .serve(routes_hello.into_make_service())
+//         .await
+//         .unwrap();
+
+//     // endregion:    --- Start server
+// }
+
+async fn hello() -> &'static str {
+    "Hello <strong>World!!!</strong>"
 }
